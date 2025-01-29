2041202f8cf5551c90ff7c5eec29e971bb1fdeaf
unknown <adityasatriapratama29@gmail.com>
2025-01-29 10:53:52 +0700

    Initial commit

---
+++ src/page/index.js
@@ -0,0 +1,6 @@
+import Home from "./Home";
+import About from "./About";
+import Projects from "./Projects";
+import Experiences from "./Experiences";
+
+export {Home, About, Projects, Experiences};
