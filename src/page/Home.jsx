6b04505d2cf538bcb8a925ab397561946248a696
unknown <adityasatriapratama29@gmail.com>
2025-02-02 23:11:40 +0700

    smooth

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,59 +1,45 @@
-import {useState, Suspense} from 'react';
-import { Canvas } from '@react-three/fiber'
-import Loader from '../components/Loader'
-
+import { useState, Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { PerformanceMonitor, Sky } from '@react-three/drei';
+import Loader from '../components/Loader';
 import Island from '../models/Island';
-import { Sky } from '@react-three/drei';
-
-
-    {/*<div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center"> 
-        POPUP
-      </div> */}
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
-
-  const adjustIslandForScreenSize = () => {
-    let screenScale = null;
-    let screenPosition = [0, -6.5, -43];
-    let rotation = [0.1, 4.7, 0]
-
-    if(window.innerWidth < 768) {
-      screenScale = [2, 2, 2];
-    } else {
-      screenScale = [1,1,1];
-    }
-
-    return[screenScale, screenPosition, rotation];
-  }
-
-
+  const [currentStage, setCurrentStage] = useState(null);
   const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
 
   return (
-    <section className='w-full h-screen relative'>
+    <section className="w-full h-screen relative">
       <Canvas
-      className={`w-full h-screen bg-transparent ${isRotating ?
-        'cursor-grabbing' : 'cursor-grab'}`}
-      camera={{near: 0.1, far: 1000 }}
+        className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
+        camera={{ near: 0.1, far: 1000 }}
       >
-        <Suspense fallback={<Loader/>}>
-        <directionalLight position={[7, 1, 1]} intensity={4}/>
-        <ambientLight intensity={0.7} />
-        <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1}/>
-
-        <Sky />
-        <Island
-        position={islandPosition}
-        scale={islandScale}
-        rotation={islandRotation}
-        isRotating={isRotating}
-        setIsRotating={setIsRotating}
-        />
+        <PerformanceMonitor />
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[7, 1, 1]} intensity={4} />
+          <ambientLight intensity={0.7} />
+          <hemisphereLight skyColor="#0068FAFF" groundColor="#000000" intensity={1} />
+          <Sky />
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+            setCurrentStage={setCurrentStage}
+          />
         </Suspense>
       </Canvas>
     </section>
-  )
-}
+  );
+};
+
+const adjustIslandForScreenSize = () => {
+  let screenScale = window.innerWidth < 768 ? [2, 2, 2] : [1, 1, 1];
+  let screenPosition = [0, -6.5, -43];
+  let rotation = [0.1, 4.7, 0];
+  return [screenScale, screenPosition, rotation];
+};
 
 export default Home;
